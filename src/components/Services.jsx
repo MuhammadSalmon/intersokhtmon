9247ad96e323101c117720417b5088fec68a9135
Muhammadsalmon <saidovsomon123@gmail.com>
2024-12-25 22:04:09 -0800

    Эук

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -99,7 +99,7 @@ const ServicesPage = () => {
     <section className="py-12 bg-gray-100 dark:bg-gray-900">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-gray-900 text-center dark:text-gray-100 mb-6">
-        НАШИ УСЛУГ
+        НАШИ УСЛУГИ
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {displayedProjects.map((project, index) => (
